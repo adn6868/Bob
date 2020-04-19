8b873b1110b58d3fea4c6ff0dbf171cf3337225a
adn6868 <44442570+adn6868@users.noreply.github.com>
2020-04-19 09:13:47 -0500

    fix logger

--- logger/logger.py
+++ logger/logger.py
@@ -1,12 +1,15 @@
 import datetime
 import sys
 import pprint as pp
+
+
 class Logger(object):
-    def __init__ (self, name =__name__):
+    def __init__(self, name=__file__):
         self.name = name
-    def getTime(self, tz = None):
-        return str(datetime.datetime.now(tz = tz))
-        
+
+    def getTime(self, tz=None):
+        return str(datetime.datetime.now(tz=tz))
+
     def info(self, data):
         try:
             data = str(data)
@@ -14,19 +17,12 @@ class Logger(object):
             raise IOError
         rv = " {}: {}: {}\n".format(self.getTime(), self.name, data)
         sys.stdout.write(rv)
+
     def infoB(self, uglyData):
         rv = " {}: {}:\n {}\n".format(self.getTime(), self.name, beautyData)
         sys.stdout.write(beautyData)
 
 
-def foo(name):
-    def __init__(self,name):
-        self.name = name
-    def getName(self):
-        return self.name
-
-
-if __name__ == '__main__':
-    a = foo("anh")
-    print(a.getName())
-
+if __name__ == "__main__":
+    log = Logger()
+    log.info("Hi world")
